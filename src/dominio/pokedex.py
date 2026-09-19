d60364d31a96d185fd2bafd38825e0edb2c585df
thiagooesq <thiago.ne008@gmail.com>
2026-09-19 15:53:04 -0300

    pokedex.py

--- src/dominio/pokedex.py
+++ src/dominio/pokedex.py
@@ -1,21 +1,22 @@
 from src.dominio.pokemon import pokemon 
 
 catalogo = [
-  pokemon ("Bulbasaur" , "Planta")
-  pokemon ("Ivysaur" , "Planta")
-  pokemon ("Venusaur" , "Planta")
-  pokemon ("Charmander" , "Fuego")
-  pokemon ("Charmeleon" , "Fuego")
-  pokemon ("Charizard" , "Fuego")
-  pokemon ("Squirtle" , "Agua")
-  pokemon ("Wartortle" , "Agua")
-  pokemon ("Blastoise" , "Agua")
-  pokemon ("Pikachu" , "Electrico")
-  pokemon ("Raichu" , "Electrico")
-  pokemon ("Eevee" , "Normal")
-  pokemon ("Vaporeon" , "Agua")
-  pokemon ("Jolteon" , "Electrico")
-  pokemon ("Flareon" , "Fuego")
+  pokemon ("Bulbasaur" , "Planta"),
+  pokemon ("Ivysaur" , "Planta"),
+  pokemon ("Venusaur" , "Planta"),
+  pokemon ("Charmander" , "Fuego"),
+  pokemon ("Charmeleon" , "Fuego"),
+  pokemon ("Charizard" , "Fuego"),
+  pokemon ("Squirtle" , "Agua"),
+  pokemon ("Wartortle" , "Agua"),
+  pokemon ("Blastoise" , "Agua"),
+  pokemon ("Pichu" , "Electrico"),
+  pokemon ("Pikachu" , "Electrico"),
+  pokemon ("Raichu" , "Electrico"),
+  pokemon ("Eevee" , "Normal"),
+  pokemon ("Vaporeon" , "Agua"),
+  pokemon ("Jolteon" , "Electrico"),
+  pokemon ("Flareon" , "Fuego"),
 ]
 
 def listar_catalogo ():
